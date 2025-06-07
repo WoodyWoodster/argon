44d22237b7b2f53e2b429363cd31fc3c084f5554
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2025-06-06 22:10:44 -0500

    Generated graphql

--- apps/api/config/initializers/cors.rb
+++ apps/api/config/initializers/cors.rb
@@ -14,3 +14,10 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:5173' # or whatever port your frontend runs on
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+  end
+end
